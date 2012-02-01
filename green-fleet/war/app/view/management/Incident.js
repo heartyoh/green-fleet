98c8e918e59def3fb71014e645d7a974f9100db3
heartyoh <heartyoh@gmail.com>
2012-02-01 15:10:57 +0900

    x

--- green-fleet/war/app/view/management/Incident.js
+++ green-fleet/war/app/view/management/Incident.js
@@ -34,7 +34,8 @@ Ext.define('GreenFleet.view.management.Incident', {
 				dataIndex : 'incidentTime',
 				text : 'Incident Time',
 				xtype : 'datecolumn',
-				format : F('datetime')
+				format : F('datetime'),
+				width : 120
 			}, {
 				dataIndex : 'driver',
 				text : 'Driver',
