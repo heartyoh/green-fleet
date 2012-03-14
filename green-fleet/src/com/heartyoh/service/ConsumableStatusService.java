7b0a6134d4b807e405d1fd8da764f381677c496d
jhnam <maparam419@gmail.com>
2012-03-15 00:27:11 +0900

    소모품 이력, 상태, 정비 이력 서비스 추가

--- green-fleet/src/com/heartyoh/service/ConsumableStatusService.java
+++ green-fleet/src/com/heartyoh/service/ConsumableStatusService.java
@@ -9,9 +9,6 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,32 +20,26 @@ import com.google.appengine.api.datastore.Key;
 import com.heartyoh.util.SessionUtils;
 
 /**
- * 차량 소모품 교체 이력 서비스
+ * 차량 소모품의 현재 상태 정보 
  * 
- * @author jhnam
+ * @author jonghonam
  */
-@Controller
-public class ConsumableChangeHistoryService extends EntityService {
+public class ConsumableStatusService extends EntityService {
 
-	private static final Logger logger = LoggerFactory.getLogger(ConsumableChangeHistoryService.class);
-			
 	@Override
 	protected String getEntityName() {
-		return "ConsumableChangeHistory";
+		return "ConsumableStatus";
 	}
 
 	@Override
 	protected String getIdValue(Map<String, Object> map) {
-		return map.get("vehicle_id") + "@" + map.get("consumable_item") + "@" + map.get("repl_date");
+		return map.get("vehicle_id") + "@" + map.get("consumable_item");
 	}
-	
+
 	@Override
 	protected void onCreate(Entity entity, Map<String, Object> map, DatastoreService datastore) throws Exception {
-		Entity company = datastore.get((Key)map.get("_company_key"));
 		entity.setProperty("vehicle_id", map.get("vehicle_id"));
-		entity.setProperty("consumable_item", map.get("consumable_item"));		
-		Date replDate = SessionUtils.stringToDateTime((String)map.get("datetime"), null, Integer.parseInt((String)company.getProperty("timezone")));
-		entity.setProperty("repl_date", replDate);
+		entity.setProperty("consumable_item", map.get("consumable_item"));
 		
 		super.onCreate(entity, map, datastore);
 	}
@@ -56,46 +47,41 @@ public class ConsumableChangeHistoryService extends EntityService {
 	@Override
 	protected void onSave(Entity entity, Map<String, Object> map, DatastoreService datastore) throws Exception {
 		
-		// 교체 시점에서의 마일리지 
-		entity.setProperty("repl_mileage", map.get("repl_mileage"));
-		// 다음 교체 예정 마일리지 
+		// 마지막(최근) 교체일 
+		entity.setProperty("last_repl_date", map.get("last_repl_date"));
+		// 최근 교체시점에서의 마일리지 
+		entity.setProperty("last_repl_mileage", map.get("last_repl_mileage"));
+		// 다음 교체시점의 마일리지 
 		entity.setProperty("next_repl_mileage", map.get("next_repl_mileage"));
-		// 다음 교체 예정 날짜 
+		// 다음 교체일 
 		entity.setProperty("next_repl_date", map.get("next_repl_date"));
-		// 교체 비용 
-		entity.setProperty("cost", map.get("cost"));
-		// 교체 작업자 
-		entity.setProperty("worker", map.get("worker"));
-		// 교체 부품 
-		entity.setProperty("component", map.get("component"));
-		// 코멘트 
-		entity.setProperty("comment", map.get("comment"));
+		// 누적 비용 
+		entity.setProperty("accum_cost", map.get("accum_cost"));
 		
 		super.onSave(entity, map, datastore);
 	}
 	
-	@RequestMapping(value = "/consumable_change/import", method = RequestMethod.POST)
+	@RequestMapping(value = "/consumable_status/import", method = RequestMethod.POST)
 	public @ResponseBody
 	String imports(MultipartHttpServletRequest request, HttpServletResponse response) throws Exception {
 		return super.imports(request, response);
 	}
 
-	@RequestMapping(value = "/consumable_change/save", method = RequestMethod.POST)
+	@RequestMapping(value = "/consumable_status/save", method = RequestMethod.POST)
 	public @ResponseBody
 	String save(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		return super.save(request, response);
 	}
 
-	@RequestMapping(value = "/consumable_change/delete", method = RequestMethod.POST)
+	@RequestMapping(value = "/consumable_status/delete", method = RequestMethod.POST)
 	public @ResponseBody
 	String delete(HttpServletRequest request, HttpServletResponse response) {
 		return super.delete(request, response);
 	}
 
-	@RequestMapping(value = "/consumable_change", method = RequestMethod.GET)
+	@RequestMapping(value = "/consumable_status", method = RequestMethod.GET)
 	public @ResponseBody
 	Map<String, Object> retrieve(HttpServletRequest request, HttpServletResponse response) {		
 		return super.retrieve(request, response);
 	}	
-
 }
---
+++ green-fleet/src/com/heartyoh/service/RepairHistoryService.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package com.heartyoh.service;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.Entity;
+
+/**
+ * 차량 Maintenence 이력 서비스  
+ * 
+ * @author jonghonam
+ */
+public class RepairHistoryService extends EntityService {
+
+	@Override
+	protected String getEntityName() {
+		return "RepairHistory";
+	}
+
+	@Override
+	protected String getIdValue(Map<String, Object> map) {		
+		return map.get("vehicle_id") + "@" + map.get("maint_date");
+	}
+
+	@Override
+	protected void onCreate(Entity entity, Map<String, Object> map, DatastoreService datastore) throws Exception {
+		entity.setProperty("vehicle_id", map.get("vehicle_id"));
+		entity.setProperty("repair_date", map.get("repair_date"));
+		
+		super.onCreate(entity, map, datastore);
+	}
+	
+	@Override
+	protected void onSave(Entity entity, Map<String, Object> map, DatastoreService datastore) throws Exception {
+		
+		// 수리 내용 
+		entity.setProperty("repair_content", map.get("repair_content"));
+		// 자동차 정비소  
+		entity.setProperty("repair_shop", map.get("repair_shop"));
+		// 자동차 수리사  
+		entity.setProperty("repair_man", map.get("repair_man"));
+		// 정비 가격 
+		entity.setProperty("repair_cost", map.get("repair_cost"));
+		// 코멘트 
+		entity.setProperty("comment", map.get("comment"));
+		
+		super.onSave(entity, map, datastore);
+	}
+	
+	@RequestMapping(value = "/repair_history/import", method = RequestMethod.POST)
+	public @ResponseBody
+	String imports(MultipartHttpServletRequest request, HttpServletResponse response) throws Exception {
+		return super.imports(request, response);
+	}
+
+	@RequestMapping(value = "/repair_history/save", method = RequestMethod.POST)
+	public @ResponseBody
+	String save(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return super.save(request, response);
+	}
+
+	@RequestMapping(value = "/repair_history/delete", method = RequestMethod.POST)
+	public @ResponseBody
+	String delete(HttpServletRequest request, HttpServletResponse response) {
+		return super.delete(request, response);
+	}
+
+	@RequestMapping(value = "/repair_history", method = RequestMethod.GET)
+	public @ResponseBody
+	Map<String, Object> retrieve(HttpServletRequest request, HttpServletResponse response) {		
+		return super.retrieve(request, response);
+	}	
+}
