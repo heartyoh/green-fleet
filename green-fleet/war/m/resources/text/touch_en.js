a03fba795ed9bc337a7c3899dfeb1100703e682b
heartyoh <heartyoh@gmail.com>
2012-06-07 20:23:43 +0900

    mobile commit.

---
+++ green-fleet/war/m/resources/text/touch_en.js
@@ -0,0 +1,12 @@
+T({
+	label : {
+		version : 'Version',
+		user : 'User',
+		run_distance : 'Run Distance',
+		consumption : 'Consumption',
+		maint_count : 'Maint. Count',
+		maint_time : 'Maint. Time',
+		co2_emission : 'CO2 Emission',
+		oos_count : 'OOS Count'
+	}
+});
