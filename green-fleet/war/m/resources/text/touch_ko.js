66dff8d2596a35036337050087727499067ca843
heartyoh <heartyoh@gmail.com>
2012-06-12 07:55:50 +0900

    incident confirm button.

--- green-fleet/war/m/resources/text/touch_ko.js
+++ green-fleet/war/m/resources/text/touch_ko.js
@@ -1,1 +1,1 @@
-/*40d7831104139fee51bc68ab276c0790504d2d73*/T({label:{version:"버전",user:"사용자",run_distance:"주행거리",consumption:"연료소모량",maint_count:"정비 횟수",maint_time:"정비 시간",co2_emission:"CO2 배출량",oos_count:"OOS 횟수",dodriver:"운전자 없음"}});
+/*81ce1f2ec743010b15563abe42cebe1180d6d892*/T({label:{version:"버전",user:"사용자",run_distance:"주행거리",consumption:"연료소모량",maint_count:"정비 횟수",maint_time:"정비 시간",co2_emission:"CO2 배출량",oos_count:"OOS 횟수",dodriver:"운전자 없음",system_info:"시스템/사용자 정보",setting_by_screen:"현재화면 관련 설정",fromYear:"조회 기준 시작 년도"}});
