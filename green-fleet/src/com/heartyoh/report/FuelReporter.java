d20775cea4982b38f271ecca9401842e77906cb3
glorys220 <glorys220@gmail.com>
2012-07-03 21:23:10 +0900

    efficiencyReport(type=report)

--- green-fleet/src/com/heartyoh/report/FuelReporter.java
+++ green-fleet/src/com/heartyoh/report/FuelReporter.java
@@ -5,11 +5,14 @@ package com.heartyoh.report;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.heartyoh.util.DataUtils;
 import com.heartyoh.util.DatasourceUtils;
+import com.heartyoh.util.GreenFleetConstant;
 
 /**
  * 연료/연비 관련 리포터 
@@ -29,7 +32,7 @@ public class FuelReporter extends AbstractReporter {
 	/**
 	 * parameter names
 	 */
-	private static final String[] PARAM_NAMES = new String[] { "company", "_today" };
+	private static final String[] PARAM_NAMES = new String[] { "company", "_today", "type" };
 	
 	@Override
 	public String getId() {
@@ -59,23 +62,68 @@ public class FuelReporter extends AbstractReporter {
 	 * @throws Exception
 	 */
 	private List<Object> averagefuelEffcc(Map<String, Object> params) throws Exception {
-				
-		StringBuffer sql = new StringBuffer();
-		sql.append("select a.vehicle, case when a.count = 0 then 0 else round((a.total / a.count), 2) end effcc ");
-		sql.append("from (");
-		sql.append("select vehicle, sum(effcc) as total, count(effcc) as count from vehicle_run_sum where company = :company and year = :year group by vehicle");
-		sql.append(") a");
 		
-		Map<String, Object> paramMap = DataUtils.newMap("company", params.get("company"));
-		int year = paramMap.containsKey("year") ? DataUtils.toInt(paramMap.get("year")) : Calendar.getInstance().get(Calendar.YEAR);
-		paramMap.put("year", year);
-		List<?> items = DatasourceUtils.selectBySql(sql.toString(), paramMap);
+		String type = (String)params.get("type");
 		
-		List<Object> results = new ArrayList<Object>();
-		for(Object item : items) {
-			results.add(item);
+		
+		if((type != null) && type.equals("report")){
+			String durationStr = (String)params.get("duration");
+			int duration = 12;
+			
+			if(!DataUtils.isEmpty(durationStr)) {
+				duration = Integer.parseInt(durationStr);
+			}
+			
+			Calendar c = Calendar.getInstance();
+			String toDateStr = c.get(Calendar.YEAR) + "-" + c.get(Calendar.MONTH) + "-28";
+			c.add(Calendar.MONTH, -(duration - 1));
+			String fromDateStr = c.get(Calendar.YEAR) + "-" + c.get(Calendar.MONTH) + "-01";		
+			Date fromDate = DataUtils.toDate(fromDateStr, GreenFleetConstant.DEFAULT_DATE_FORMAT);
+			Date toDate = DataUtils.toDate(toDateStr, GreenFleetConstant.DEFAULT_DATE_FORMAT);
+			
+			
+//			select year, month, format(sum(effcc) / count(company), 2) as effcc, format(sum(consmpt) / count(company), 2) as consmpt from vehicle_run_sum where company='palmvision' and month_date >= '2011-07-01' and month_date <= '2012-06-30' group by year, month
+			
+			StringBuffer sql = new StringBuffer();
+			sql.append("select ");
+			sql.append("year, month, format(sum(effcc) / count(company), 2) effcc, format(sum(consmpt) / count(company), 2) consmpt ");
+			sql.append("from ");
+			sql.append("vehicle_run_sum ");
+			sql.append("where ");
+			sql.append("company = :company and month_date >= :fromDate and month_date <= :toDate group by year, month");
+			
+			Map<String, Object> paramMap = DataUtils.newMap("company", params.get("company"));
+			paramMap.put("fromDate", fromDate);
+			paramMap.put("toDate", toDate);
+			List<?> items = DatasourceUtils.selectBySql(sql.toString(), paramMap);
+			
+			List<Object> results = new ArrayList<Object>();
+			for(Object item : items) {
+				results.add(item);
+			}
+			
+			return results;
+			
+		}else{
+			StringBuffer sql = new StringBuffer();
+			sql.append("select a.vehicle, case when a.count = 0 then 0 else round((a.total / a.count), 2) end effcc ");
+			sql.append("from (");
+			sql.append("select vehicle, sum(effcc) as total, count(effcc) as count from vehicle_run_sum where company = :company and year = :year group by vehicle");
+			sql.append(") a");
+			
+			Map<String, Object> paramMap = DataUtils.newMap("company", params.get("company"));
+			int year = paramMap.containsKey("year") ? DataUtils.toInt(paramMap.get("year")) : Calendar.getInstance().get(Calendar.YEAR);
+			paramMap.put("year", year);
+			List<?> items = DatasourceUtils.selectBySql(sql.toString(), paramMap);
+			
+			List<Object> results = new ArrayList<Object>();
+			for(Object item : items) {
+				results.add(item);
+			}
+			return results;
 		}
-		return results;
+				
+		
 	}
 
 }
